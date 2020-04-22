f094faa19a54ba900907bc8e59d76863dd2c1567
i.tkachev <itkachev@vk.com>
2020-04-22 17:21:29 +0300

    Updates

---
+++ precache-manifest.b7189765133da682032296b8628b3d28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f3929b3a66636e43378794c4bc733a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97efc11291327c7ecbd1",
+    "url": "./static/css/2.9e627a5a.chunk.css"
+  },
+  {
+    "revision": "905e83cba919fdeca964",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "97efc11291327c7ecbd1",
+    "url": "./static/js/2.d7e04b49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d7e04b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905e83cba919fdeca964",
+    "url": "./static/js/main.57683432.chunk.js"
+  },
+  {
+    "revision": "11dcc61891ac861d539f",
+    "url": "./static/js/runtime-main.b7bbd5e1.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
